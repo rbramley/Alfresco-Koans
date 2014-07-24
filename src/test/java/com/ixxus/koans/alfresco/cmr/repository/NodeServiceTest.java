1dfa5a0342a49946169cf5895e52d0fa7278361c
Alex Lu <alex.lu@contractor.cengage.com>
2014-07-24 15:00:55 +0100

    Created a test case teaching how to createStore and getStores

--- src/test/java/com/ixxus/koans/alfresco/cmr/repository/NodeServiceTest.java
+++ src/test/java/com/ixxus/koans/alfresco/cmr/repository/NodeServiceTest.java
@@ -1,13 +1,19 @@
 package com.ixxus.koans.alfresco.cmr.repository;
 
 import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.repository.StoreRef;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Alex Lu
@@ -19,27 +25,48 @@ public class NodeServiceTest {
     private NodeService nodeService;
 
     @InjectMocks
-    private CreateStore createStore;
+    private Store store;
 
     @Test
-    public void createStoreTest() {
-        String protocol = "foo";
-        String identifier = "bar";
-        createStore.createStore(protocol, identifier);
+    public void ensureNodeServiceCreateStoreIsInvoked() {
+        StoreRef storeRef = StoreRef.STORE_REF_WORKSPACE_SPACESSTORE;
 
-        verify(nodeService).createStore(protocol, identifier);
+        /// ==== Your code starts here === ///
+
+        /// ==== Your code ends here === ///
+
+        verify(nodeService).createStore(storeRef.getProtocol(), storeRef.getIdentifier());
+    }
+
+    @Test
+    public void ensureNodeServiceReturnsStore() {
+        StoreRef storeRef = StoreRef.STORE_REF_WORKSPACE_SPACESSTORE;
+        List<StoreRef> expectedStoreRefs = Arrays.asList(storeRef);
+        when(nodeService.getStores()).thenReturn(expectedStoreRefs);
+
+        List<StoreRef> actualStoreRefs = null;
+
+        /// ==== Your code starts here === ///
+
+        /// ==== Your code ends here === ///
+
+        assertTrue(storeRef + " should be returned", actualStoreRefs.contains(storeRef));
     }
 
 }
 
-class CreateStore {
+class Store {
     private NodeService nodeService;
 
-    CreateStore(NodeService nodeService) {
+    Store(NodeService nodeService) {
         this.nodeService = nodeService;
     }
 
     public void createStore(String protocol, String identifier) {
         this.nodeService.createStore(protocol, identifier);
+    }
+
+    public List<StoreRef> getStores() {
+        return this.nodeService.getStores();
     }
 }
